c7ae5ca98bab1cc20eb38a40e329f9aad2e5b78c
duxin14436106 <1284573827@qq.com>
2019-06-05 00:26:12 +0800

    完善头部组件

--- vue-bangbang/src/components/m-index/headerAll.vue
+++ vue-bangbang/src/components/m-index/headerAll.vue
@@ -1,40 +1,75 @@
 <template>
   <div style="margin: 0 auto">
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal">
-      <template v-for="(item) of navs">
-        <el-menu-item :index="item.number + ''">{{item.name}}</el-menu-item>
-      </template>
+      <img src="../../assets/img/BBTlogo.jpg"/>
+      <el-menu-item v-for="(item,index) of navs" :key="index" :index="item.number + ''">{{item.name}}</el-menu-item>
+      <div class="el-logins">
+        <i class="el-icon-user"></i>
+      </div>
+      <i class="el-icon-arrow-down el-left"></i>
+      <el-button size="mini" round>超小按钮</el-button>
+      <span class="el-left el-layout">[退出]</span>
     </el-menu>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "headerAll",
-    data() {
-      return {
-        navs:[
-          {number:1,name:'首页'},
-          {number:2,name:'组队CP'},
-          {number:3,name:'学习日记'},
-          {number:4,name:'打卡记录'},
-          {number:5,name:'练习项目'},
-          {number:6,name:'就业模式'},
-          {number:7,name:'代练模块'}
-        ],
-        activeIndex: '1',
-        activeIndex2: '1'
-      };
-    },
-    methods: {
-
+export default {
+  name: 'headerAll',
+  data () {
+    return {
+      navs: [
+        {number: 1, name: '首页'},
+        {number: 2, name: '组队CP'},
+        {number: 3, name: '学习日记'},
+        {number: 4, name: '打卡记录'},
+        {number: 5, name: '练习项目'},
+        {number: 6, name: '就业模式'},
+        {number: 7, name: '代练模块'}
+      ],
+      activeIndex: '1',
+      activeIndex2: '1'
     }
+  },
+  methods: {
+
   }
+}
 </script>
 
-<style scoped>
+<style scoped lang="less">
   .el-menu-demo{
     display: flex;
     justify-content: center;
+    img {
+      margin-right: 20px;
+      width: 5%;
+      height: 5%;
+    }
+    i{
+      display: flex;
+      align-items: center;
+      .el-left{
+        margin-left: 5px;
+      }
+    }
+    .el-logins{
+      margin: 20px 5px 20px 50px;
+      display: flex;
+      width:20px;
+      height: 20px;
+      border:solid 1px #909399;
+      border-radius: 50%;
+      justify-content: center;
+      align-items: center;
+    }
+    .el-button{
+      padding: 10px;
+      height: 30px;
+      margin: 15px;
+    }
+    .el-layout{
+      padding: 20px 0;
+    }
   }
 </style>
